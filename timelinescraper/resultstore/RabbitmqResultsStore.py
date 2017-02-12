f6028e3ff3cda46dcbf06355c81d7fd99410459c
Giuseppe Galano <galanogiuseppe@gmail.com>
2017-02-12 13:18:36 +0100

    rabbitmq results store is now using queues

--- timelinescraper/resultstore/RabbitmqResultsStore.py
+++ timelinescraper/resultstore/RabbitmqResultsStore.py
@@ -5,18 +5,18 @@ class RabbitmqResultsStore(object):
     
     @staticmethod
     def get_config_params():
-        return [{"name":"rabbitmq_exchange_name", "type":"String"}]
+        return [{"name":"queue_name", "type":"String"}]
 
-    def __init__(self, name, workspace, rabbitmq_exchange_name):    
+    def __init__(self, name, workspace, queue_name):    
         self.logger = logging.getLogger(name)
-        self.rabbitmq_exchange_name = rabbitmq_exchange_name
-
+        self.queue_name = queue_name
 
         self.connection = pika.BlockingConnection(
                 pika.ConnectionParameters(host='localhost', heartbeat_interval=0)
              )
+
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.rabbitmq_exchange_name, type='fanout')
+        self.channel.queue_declare(queue=queue_name)
 
     def __del__(self):
         self.connection.close()
@@ -28,20 +28,14 @@ class RabbitmqResultsStore(object):
     # Stores string as a newline in the output file
     def store_string(self,string_to_store):
         self.channel.basic_publish(
-            exchange=self.rabbitmq_exchange_name, 
-            routing_key='', 
+            exchange='', 
+            routing_key=self.queue_name, 
             body=string_to_store,
             properties = pika.BasicProperties(content_type='application/json'))
         
     # Kept for legacy, deprecated. Use store_dict_as_json or store_string
     def store(self,result):
-        self.logger.warning("FileSystemResultsStore.store is deprecated")
-        # Get lock to synchronize threads
-        # self.threadLock.acquire()
-        json_encoded_status = json.dumps(result, sort_keys=True)
-        # print("storing result",json_encoded_status)
-        self.output_file.write(json_encoded_status+'\n')
-        # Free lock to release next thread
-        # self.threadLock.release()
+        self.logger.error("FileSystemResultsStore.store is deprecated")
+        pass
 
 
